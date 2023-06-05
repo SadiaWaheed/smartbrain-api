5f14862fd60344e2755889162a75826af9a984cd
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-06-05 19:15:42 +0300

    using transactions to insert data into user and login table for registering users

--- server.js
+++ server.js
@@ -66,17 +66,32 @@ app.post("/signin", (req, res) => {
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
 
-  db("users")
-    .returning("*")
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("unable to register."));
+  var hash = bcrypt.hashSync(password);
+
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  })
+  .catch((err) => res.status(400).json("unable to register."));
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -103,7 +118,7 @@ app.put("/image", (req, res) => {
     .then((entries) => {
       res.json(entries[0].entries);
     })
-    .catch(err => res.status(400).json('unable to get entries'));
+    .catch((err) => res.status(400).json("unable to get entries"));
 });
 
 // Load hash from your password DB.
