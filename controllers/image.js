b56738b569eaad565c38546a150860208f6dd867
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-06-07 20:10:36 +0300

    debugging image.js

--- controllers/image.js
+++ controllers/image.js
@@ -44,6 +44,7 @@ const handleApiCall = (req, res) => {
   )
     .then((response) => response.json())
     .then((data) => {
+      console.log(data)
       res.json(data);
     })
     .catch((err) => res.status(400).json(err));
