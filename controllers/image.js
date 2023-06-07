7ee39671f647985575bf48298d1c858116ac3cc3
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-06-07 21:02:51 +0300

    fetch import another way

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,5 @@
-const fetch = require("node-fetch");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const returnClarifaiRequestOptions = (imageUrl) => {
   // Your PAT (Personal Access Token) can be found in the portal under Authentification
